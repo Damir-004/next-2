f1fdb98dd5febbcc49cf58041f44ad09ce0d2af8
Damir Bakhtiyarov <125185359+Damir-004@users.noreply.github.com>
2023-09-17 10:52:18 +0500

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,8 +7,6 @@ const Header = () => {
                 <Link href="/">Home</Link>
                 <Link href="/electronics">electronics</Link>
                 <Link href="/jewelery">jewelery</Link>
-                <Link href="/men">men's clothing</Link>
-                <Link href="/women">women's clothing</Link>
             </div>
         </header>
     )
