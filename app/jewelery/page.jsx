5ff87e514424ba187ecb90d7e34867c0d3af1b8b
Damir Bakhtiyarov <125185359+Damir-004@users.noreply.github.com>
2023-09-18 20:46:40 +0500

    Update page.jsx

--- app/jewelery/page.jsx
+++ app/jewelery/page.jsx
@@ -21,7 +21,7 @@ function Posts() {
   };
   const handleUserClick = (postId) => {
     setBlockId(postId);
-    const newURL = `http://localhost:3000/users/${postId}`;
+    const newURL = `https://fancy-speculoos-78ca83.netlify.app/users/${postId}`;
     window.history.pushState(null, '', newURL);
   };
   useEffect(() => {
